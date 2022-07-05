5753dcb765bb6f5860f3ab0296777c1d540eaa47
Christopher Lightfoot <chris@wolfshark.co.uk>
2022-07-05 23:04:47 +0100

    Project header.

---
+++ include/model/Project.h
@@ -0,0 +1,35 @@
+//
+// Created by vruser on 03/07/2022.
+//
+
+#ifndef EDITOR_MODEL_PROJECT_H
+#define EDITOR_MODEL_PROJECT_H
+
+#include <vector>
+#include "nlohmann/json.hpp"
+
+#include "Palette.h"
+
+
+namespace Model {
+
+    class Project {
+    public:
+        const std::string &getName() const;
+
+        void setName(const std::string &mName);
+
+        const std::vector<Palette> &getPalettes() const;
+
+        void setPalettes(const std::vector<Model::Palette> &mPalettes);
+
+    public:
+        std::string name;
+        std::vector<Model::Palette> palettes;
+    };
+
+    void to_json(nlohmann::json& j, const Project& p);
+    void from_json(const nlohmann::json& j, Project& p);
+}
+
+#endif //EDITOR_MODEL_PROJECT_H
